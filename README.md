9a56c394ae382a59c9adcda12687561397fc39bb
kobi <kobi@geekfomo.com>
2024-05-14 10:36:50 +0300

    start plugin

---
+++ README.md
@@ -0,0 +1,5 @@
+# User Faker For WordPress
+
+Let admin users to act as different users. 
+
+See [User Faker](https://geekfomo.com/user-faker-for-wordpress/) for more details.
---
+++ user-faker/user-faker.php
@@ -0,0 +1,11 @@
+<?php
+/*
+ * Plugin Name: User-Faker
+ * Plugin URI: https://geekfomo.com/user-faker-for-wordpress/
+ * Description: Switch users easily
+ * Author: Kobi
+ * Author URI: https://geekfomo.com
+ * Version: 1.0.0
+ * Requires at least: 6.0
+ * Requires PHP: 7.4
+ */
